5fcdba895fa02c8fc0b0f783efc32d9601bae10a
OuKai <apple36832@gmail.com>
2025-05-29 22:28:22 +0800

    [LiteRP] 可見對象渲染

--- Assets/LiteRP/Runtime/LiteRenderPipeline.cs
+++ Assets/LiteRP/Runtime/LiteRenderPipeline.cs
@@ -6,6 +6,8 @@ namespace LiteRP
 {
     public class LiteRenderPipeline : RenderPipeline
     {
+        private readonly static ShaderTagId _shaderTagId = new ShaderTagId("SRPDefaultUnlit");
+        
         // Old version
         protected override void Render(ScriptableRenderContext context, Camera[] cameras)
         {
@@ -44,13 +46,52 @@ namespace LiteRP
             
             // 設置相機屬性參數
             context.SetupCameraProperties(camera);
+
+            bool clearSkybox = camera.clearFlags == CameraClearFlags.Skybox;
+            bool clearDepth = camera.clearFlags != CameraClearFlags.Nothing;
+            bool clearColor = camera.clearFlags == CameraClearFlags.Color;
             
             // 清理渲染目標
+            cmd.ClearRenderTarget(clearDepth, clearColor, CoreUtils.ConvertSRGBToActiveColorSpace(camera.backgroundColor));
+
+            if (clearSkybox)
+            {
+                // 繪製天空盒
+                var skyboxRendererList = context.CreateSkyboxRendererList(camera);
+                cmd.DrawRendererList(skyboxRendererList);
+            } 
+            
             // 指定渲染排序設置SortSettings
+            var sortSettings = new SortingSettings(camera);
+            
             // 指定渲染狀態設置DrawSettings
+            var drawSettings = new DrawingSettings(_shaderTagId, sortSettings);
+
+            #region 繪製不透明物體
+
+            sortSettings.criteria = SortingCriteria.CommonOpaque;
             // 指定渲染過濾設置FilterSettings
+            var filterListSettings = new FilteringSettings(RenderQueueRange.opaque);
             // 創建渲染列表
+            var rendererListParams = new RendererListParams(cullingResults, drawSettings, filterListSettings);
+            var rendererList = context.CreateRendererList(ref rendererListParams);
             // 繪製渲染列表
+            cmd.DrawRendererList(rendererList);
+            
+            #endregion
+
+            #region 繪製透明物體
+
+            sortSettings.criteria = SortingCriteria.CommonTransparent;
+            // 指定渲染過濾設置FilterSettings
+            filterListSettings = new FilteringSettings(RenderQueueRange.transparent);
+            // 創建渲染列表
+            rendererListParams = new RendererListParams(cullingResults, drawSettings, filterListSettings);
+            rendererList = context.CreateRendererList(ref rendererListParams);
+            // 繪製渲染列表
+            cmd.DrawRendererList(rendererList);
+
+            #endregion
             
             // 提交命令緩衝區
             context.ExecuteCommandBuffer(cmd);
